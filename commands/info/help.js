8bed2fafb650e1d2bd63a54561d7836a5f37618c
Fazlul Hoque <fazlulcse17@gmail.com>
2020-09-14 10:37:26 +0600

    added lock.js

--- commands/info/help.js
+++ commands/info/help.js
@@ -17,7 +17,9 @@ module.exports = {
             .setColor('d413ee')
             .setDescription(` ${config.prefix}say your_messsage\n${config.prefix}csay #target_channel your_message
             ${config.prefix}ban @user reason\n${config.prefix}kick @user reason\n${config.prefix}play what_to_play\n${config.prefix}mute @user time_in_hour
-            ${config.prefix}watch what_to_watch\n${config.prefix}ping \n ${config.prefix}avatar \n ${config.prefix}meme \n${config.prefix}whois`)
+            ${config.prefix}watch what_to_watch\n${config.prefix}ping \n ${config.prefix}avatar \n ${config.prefix}meme \n${config.prefix}whois
+            ${config.prefix}lock #channel_name Reason_for_Locking\n${config.prefix}unlock #channel_name\n${config.prefix}lock-server
+            ${config.prefix}unlock-server`)
             .setFooter(`requested by ${message.author.tag}`)
         message.channel.send(embed)
     }
