0127964ae6a4ebdf2f5b03d9dc16c18dae16673b
Fazlul Hoque <fazlulcse17@gmail.com>
2020-09-14 12:48:45 +0600

    added one word story channel monitor

--- index.js
+++ index.js
@@ -12,59 +12,88 @@ app.listen(port, () => console.log(`Bot listening at http://localhost:${port}`))
 
 
 
-const { Client,Collection } = require("discord.js")
+const {
+  Client,
+  Collection
+} = require("discord.js")
 
 const fs = require("fs")
-const client = new Client ({ 
-    disableEveryone:"true" // This makes sure that the bot does not mention everyone
+const client = new Client({
+  disableEveryone: "true" // This makes sure that the bot does not mention everyone
 });
 require('dotenv').config();
 client.commands = new Collection();
 client.aliases = new Collection();
 client.categories = fs.readdirSync("./commands/")
 const config = require("./config.json"); // enter your bot prefix in the config.json file
-const { settings } = require("cluster");
-const { eventNames } = require("process");
+const {
+  settings
+} = require("cluster");
+const {
+  eventNames
+} = require("process");
 const prefix = config.prefix;
-const myFunctions= require('./functions.js');
+const myFunctions = require('./functions.js');
+const monitor_channel = ["750687772813033541"]
 
-['command'].forEach(handler=>{
-    require(`./handler/${handler}`)(client);
+['command'].forEach(handler => {
+  require(`./handler/${handler}`)(client);
 })
 
 
 
 client.on("ready", async () => {
   console.log(`${client.user.tag} is online`);
-  client.user.setActivity('PUMBA\'s New Video!',{type:'WATCHING' ,url:"https://discord.gg/asdfa" }).catch(console.error);
+  client.user.setActivity('PUMBA\'s New Video!', {
+    type: 'WATCHING',
+    url: "https://discord.gg/asdfa"
+  }).catch(console.error);
 });
-  
 
 
-client.on("message",async message =>{
-  
-    if(message.channel.type=="dm" && !message.author.bot  ) {
-        if(message.content.startsWith(config.prefix))
-          return ;
-        else{
-             myFunctions.dm_received(client,message);
-        }
+
+client.on("message", async message => {
+
+  if (message.channel.type == "dm" && !message.author.bot) {
+    if (message.content.startsWith(config.prefix))
+      return;
+    else {
+      myFunctions.dm_received(client, message);
     }
-    
+  }
+  monitor_channel.forEach(item => {
+    if (item == message.channel.id)
+      channel_monitor(message);
 
-    client.prefix = prefix;
-    if(message.author.bot) return; // This line makes sure that the bot does not respond to other bots
-    if(!message.guild) return;
-    if(!message.content.startsWith(prefix)) return; // This line makes sure that the bot does not respond to other messages with the bots prefix
-    if(!message.member) message.member = await message.guild.fetchMember(message);
-    const args = message.content.slice(prefix.length).trim().split(/ +/g);
-    const cmd = args.shift().toLowerCase();
-    if(cmd.length ===0) return;
+  });
 
-    let command = client.commands.get(cmd);
-    if(!command) command = client.commands.get(client.aliases.get(cmd));
-    if(command)
-    command.run(client,message,args,cmd);
-})
 
-client.login(process.env.token)//Enter your bot token here
+  client.prefix = prefix;
+  if (message.author.bot) return; // This line makes sure that the bot does not respond to other bots
+  if (!message.guild) return;
+  if (!message.content.startsWith(prefix)) return; // This line makes sure that the bot does not respond to other messages with the bots prefix
+  if (!message.member) message.member = await message.guild.fetchMember(message);
+  const args = message.content.slice(prefix.length).trim().split(/ +/g);
+  const cmd = args.shift().toLowerCase();
+  if (cmd.length === 0) return;
+
+  let command = client.commands.get(cmd);
+  if (!command) command = client.commands.get(client.aliases.get(cmd));
+  if (command)
+    command.run(client, message, args, cmd);
+});
+
+function channel_monitor(message) {
+  if (message.channel.id == "750687772813033541") {
+    const args = message.content.split(/ +/g);
+    var fl = [];
+    args.forEach(function (item) {
+      if (item.length)
+        fl.push(item)
+    });
+    if (fl.length > 1)
+      message.delete();
+  }
+}
+
+client.login(process.env.token) //Enter your bot token here
